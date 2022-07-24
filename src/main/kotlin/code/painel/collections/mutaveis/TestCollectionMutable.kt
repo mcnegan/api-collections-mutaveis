194e677f7d219715c341faff5886a3a7f532734d
mcnegan <mcpantoj@gmail.com>
2022-07-23 21:43:51 -0300

    API Collections MutableListOfSet

--- src/main/kotlin/code/painel/collections/mutaveis/TestCollectionMutable.kt
+++ src/main/kotlin/code/painel/collections/mutaveis/TestCollectionMutable.kt
@@ -10,8 +10,27 @@ fun main() {
     val pedro = Funcionario("Pedro", 1212.0, "PJ") // Instancias
     val maria = Funcionario("Maria", 1550.0, "CLT")
 
+    println("--------Listar-------------")
     val funcionarios = mutableListOf(joao, maria)
     funcionarios.forEach { println(it) }
+
+    println("--------Adicionar-------------")
+    funcionarios.add(pedro)
+    funcionarios.forEach { println(it) }
+    println("--------Remover--------------")
+    funcionarios.remove(pedro)
+    funcionarios.forEach { println(it) }
+    println("--------Set--------------")
+    val funcionarioSet = mutableSetOf(joao)
+    //funcionarioSet.forEach { println(it) }
+
+    funcionarioSet.add(pedro)
+    funcionarioSet.add(maria)
+    funcionarioSet.forEach { println(it) }
+
+    println("--------Remove---------")
+    funcionarioSet.remove(maria)
+    funcionarioSet.forEach { println(it) }
 
 }
 
