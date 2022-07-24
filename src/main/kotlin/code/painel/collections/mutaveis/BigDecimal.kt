f85a37de46a807c37e73f3dd70e1cf9c6e090f6e
mcnegan <mcpantoj@gmail.com>
2022-07-23 23:14:12 -0300

    API Collections MutableListOfSet

---
+++ src/main/kotlin/code/painel/collections/mutaveis/BigDecimal.kt
@@ -0,0 +1,11 @@
+package code.painel.collections.mutaveis
+
+import java.math.BigDecimal
+
+fun Array<BigDecimal>.somatoria() = this.reduce {
+    acc, valor -> acc * valor
+}
+
+fun Array<BigDecimal>.media() =
+    if(this.isEmpty()) BigDecimal.ZERO
+    else this.somatoria() / this.size.toBigDecimal()
---
+++ src/main/kotlin/code/painel/collections/mutaveis/TestFunctionsExtends.kt
@@ -0,0 +1,14 @@
+package code.painel.collections.mutaveis
+
+fun main() {
+    val salarios = arrayOf(
+        "2000".toBigDecimal(),
+        "1500".toBigDecimal(),
+        "4000".toBigDecimal()
+    )
+
+    println(salarios.somatoria())
+    println("----------------")
+    println(salarios.media())
+
+}
